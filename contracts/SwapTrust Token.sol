c16444862b3227a91d50bd1dbbba5353f953d7a3
jcontrerasd <jaime.contreras.d@gmail.com>
2023-10-13 11:27:52 -0300

    Update SwapTrust Token.sol

--- contracts/SwapTrust Token.sol
+++ contracts/SwapTrust Token.sol
@@ -3,20 +3,17 @@
 // // SwapTrust Token (STT):
 
 // // Nombre del Token: SwapTrust Token (STT).
-// // Significado: SwapTrust combina las palabras "swap" (intercambio) y "trust" (confianza). Este nombre sugiere que el 
-// // token se utiliza para respaldar y registrar intercambios confiables entre los miembros de la comunidad.
-// // Descripción: El SwapTrust Token (STT) es la moneda digital que respalda la economía colaborativa en nuestra 
-// // comunidad de emprendedores. Este token se utiliza para registrar y facilitar intercambios confiables de productos 
-// // y servicios entre emprendedores A y B.
-// // Por ejemplo, si un emprendedor A quiere devolver un producto a un emprendedor B y comprar otro producto de otro emprendedor C, 
+// // Significado: SwapTrust combina las palabras "swap" (intercambio) y "trust" (confianza). 
+// // Este nombre sugiere que el token se utiliza para respaldar y registrar intercambios confiables entre los miembros de una comunidad comercial.
+// // Descripción: El SwapTrust Token (STT) es la moneda digital que respalda la economía colaborativa en de una comunidad de emprendedores.
+// // Este token se utilizará para registrar y facilitar intercambios confiables de productos y servicios entre emprendedores A y B.
+// // 
+// // Por ejemplo, si un cliente quiere devolver un Producto de un emprendedor A y comprar a un emprendedor B, 
 // // el STT sería la moneda que permitiría realizar estas transacciones de manera eficiente y confiable. 
 // // Cada STT representa un valor y una confianza en los intercambios dentro de la comunidad.
 // // Propósito: El propósito principal del SwapTrust Token (STT) es servir como una unidad de intercambio confiable que 
-// // respalda las actividades de trueque y colaboración entre los miembros de la comunidad de emprendedores. 
-// // Facilita transacciones sin problemas y ayuda a mantener un registro transparente de todas las actividades 
-// // comerciales dentro de la comunidad.
-// // Este nombre de token resalta la importancia de la confianza en los intercambios entre emprendedores, y el token STT está 
-// // diseñado para respaldar esta confiabilidad en la economía colaborativa de la comunidad.
+// // respalda las actividades de trueque y colaboración entre los miembros de la comunidad de emprendedores y sus clientes.
+// // Facilita transacciones sin problemas y ayuda a mantener un registro transparente de todas las actividades comerciales dentro de la comunidad.
 
 pragma solidity ^0.8.20;
 
